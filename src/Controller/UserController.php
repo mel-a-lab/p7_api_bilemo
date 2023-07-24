8ee71c09ddfe4dbcf2af73f2303d8550493f7e7e
mel-a-lab <melanie.dussenne@gmail.com>
2023-07-24 11:29:09 +0200

    pagination user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Account;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,16 +18,22 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserController extends AbstractController
 {
-    #[Route('/api/account/{idAccount}/users', name: 'show_user', methods: ['GET'])]
-    public function showUser(UserRepository $userRepository, int $idAccount): JsonResponse
+    #[Route('/api/account/{account}/users', name: 'show_user', methods: ['GET'])]
+    public function showUsers(UserRepository $userRepository, Request $request, Account $account, PaginatorInterface $paginator): JsonResponse
     {
-        $users = $userRepository->findBy(['account' => $idAccount]);
+        $page = $request->query->getInt('page', 1); 
+        $limit = $request->query->getInt('limit', 10); 
 
-        return $this->json($users, 200);
+        $pagination = $paginator->paginate(
+            $userRepository->findAll(),$page,$limit 
+        ); 
+        
+        $users = $userRepository->findBy(['account' => $account]);
+
+            return $this->json($users, 200);
     }
 
     #[Route('/api/account/{idAccount}/users', name: 'registration_user', methods: ['POST'])]
-    #[IsGranted('ROLE_ADMIN')]
     public function registration(
         Request $request,
         EntityManagerInterface $entityManager,
@@ -63,17 +71,14 @@ class UserController extends AbstractController
     #[Route('/api/users/{id}', name: 'api_user_details', methods: ['GET'])]
     public function getUserDetails(User $user, EntityManagerInterface $entityManager): JsonResponse
     {
-        if (!$user) {
-            return new JsonResponse(['message' => "Something is wrong with your properties"], 404);
-        } else {
+    
             $response = [
                 'id' => $user->getId(),
                 'name' => $user->getEmail(),
-                'account' => $user->getAccount(),
             ];
     
-            return $this->json(null, 204);
-        }
+            return $this->json($response, 200);
+        
     }
 
 
